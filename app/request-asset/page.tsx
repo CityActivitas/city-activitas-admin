b750d83b136bb93e993c1f9cbb869551cc4dfccd
Park Lu <chochinlu@gmail.com>
2024-11-23 21:57:32 +0800

    add the request-asset route

---
+++ app/request-asset/page.tsx
@@ -0,0 +1,5 @@
+import { RequestAsset } from "@/components/request-asset"
+
+export default function RequestAssetPage() {
+  return <RequestAsset />
+} 
---
+++ components/request-asset.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Header } from "@/components/header"
+// import { RequestAssetForm } from "@/components/request-asset-form"
+
+export function RequestAsset() {
+  return (
+    <main className="min-h-screen bg-gray-200">
+      <Header />
+      <div className="container mx-auto px-4 pt-24">
+        {/* <RequestAssetForm /> */}
+      </div>
+    </main>
+  )
+} 
